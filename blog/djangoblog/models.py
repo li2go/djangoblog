dda1ca6c4bd834ae53161f44f0f1b0aa2f7df265
baoziv587 <393380852@qq.com>
2015-03-11 02:26:14 +0800

    add ckEditor RichTextFile model

--- blog/djangoblog/models.py
+++ blog/djangoblog/models.py
@@ -1,10 +1,12 @@
+#coding:utf-8
 from django.db import models
 #from django.views.generic.list import ListView
-
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Article(models.Model):
     title =models.CharField(max_length=150)
     content =models.TextField()
+    content=RichTextField('正文')
     timestamp =models.DateTimeField()
 
