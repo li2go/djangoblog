d8d18a4def80f5cd2f7014291cd51f0d39528137
baoziv587 <393380852@qq.com>
2015-03-10 20:01:12 +0800

    settings change

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -7,7 +7,7 @@ Django settings for blog project.
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates').replace('\\','/'),)
+TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates').replace('\\','/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'djangoblog',
     'account',
     'blog',
+    'django.contrib.staticfiles'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -43,6 +44,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 )
 
 ROOT_URLCONF = 'blog.urls'
@@ -82,8 +84,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'statics_file')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static').replace('\\','/'),
     #'/var/local/blog/blog/static'
 )
- 
+#media
+MEDIA_URL = "/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media').replace('\\','/')  
+CKEDITOR_UPLOAD_PATH = "article"
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from djangoblog.views import index
 from django.contrib import admin
+from djangoblog.api import ArticleResource
+import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/',include('account.urls')),
-    url(r'^$',include('djangoblog.urls'))
+    url(r'^$',include('djangoblog.urls')),
+    url(r'^api/', include(ArticleResource().urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),    
+    url(r'^ckeditor/', include('ckeditor.urls'))  ,
 )
