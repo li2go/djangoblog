b0e7c23201f3408b33a1211f231369d76f639c22
baoziv587 <393380852@qq.com>
2015-03-10 20:04:52 +0800

    admin templates and RichTextEditor

--- blog/djangoblog/urls.py
+++ blog/djangoblog/urls.py
@@ -1,7 +1,9 @@
 from djangoblog import views
 from django.conf.urls import include,url,patterns
+from djangoblog.api import ArticleResource
 
 urlpatterns = patterns('',
 	url(r'^$',views.index),
+	#url(r'^Article', include(ArticleResource().urls)),
 
 	)
--- blog/static/js/tpl.js
+++ blog/static/js/tpl.js
@@ -3,14 +3,25 @@
  * template and template render.
  */
 
-var tpl = function(tpl){
-    this.tpl=tpl;
+var tpl = function(tpl,ArticleApi){
+    var api="http://localhost:8000/api/Article/?format=json";
+    this.ArticleApi=ArticleApi? ArticleApi:api,
+    this.tpl=tpl
+}
+
+ //  JSON={title,content,timestap}
+var template
+//Rrender.
+tpl.prototype.render=function(){
 
 }
 
-tpl.prototype={
-    //RENDER TPL
-    render: function(){
-
-    }
+tpl.prototype.getArticle=function(){
+    jsondata=$.get(this.ArticleApi);
+    data = $.parseJSON(jsondata);
+    return data;
 }
+
+/*
+a=new tpl("<div></div>");
+console.log(a.ArticleApi);*/
