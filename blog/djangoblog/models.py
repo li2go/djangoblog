dae96971653917c5dcb189a35c27682bb52ad96a
baoziv587 <393380852@qq.com>
2015-03-15 01:58:06 +0800

    add article like

--- blog/djangoblog/models.py
+++ blog/djangoblog/models.py
@@ -1,5 +1,7 @@
 #coding:utf-8
 from django.db import models
+from django.db.models import F
+
 #from django.views.generic.list import ListView
 from ckeditor.fields import RichTextField
 
@@ -9,4 +11,13 @@ class Article(models.Model):
     content =models.TextField()
     content=RichTextField('正文')
     timestamp =models.DateTimeField()
+    like = models.IntegerField()
+    age = models.CharField(max_length=150)
 
+    def add_like(self,article_id):
+    	article = self.objects.get(like=article_id)
+    	article.like = F('like')+1
+    	article.save()
+
+
+    
